5c27a9d4b747ce8448323111faa7089c8b0d20d1
Nick Dubus <nico_dubus@hotmail.com>
2014-10-28 23:35:30 -0400

    loaded positional arrays

--- main.rb
+++ main.rb
@@ -3,31 +3,60 @@ require 'csv'
 require "./Player.rb"
 require "./data.rb"
 require "./Roster.rb"
+require "./helperMethods.rb"
 
 #load players into hash
 players = Hash.new
 keys = ['position', 'name', 'ppg', 'gp', 'matchup', 'salary', 'exp+', 'expPts', 'dpp']
 players = CSV.parse($data).map {|a| Hash[ keys.zip(a) ] }
 
-	qb = players.detect {|f| f["position"] == "QB" && f["dpp"] < "500"}
-	wr1 = players.detect {|f| f["position"] == "WR"  && f["dpp"] < "500"}
-	wr2 = players.detect {|f| f["position"] == "WR"  && f["name"] != wr1["name"] && f["dpp"] < "500"}
-	wr3= players.detect {|f| f["position"] == "WR"  && f["name"] != wr1["name"] && f["name"] != wr2["name"] && f["dpp"] < "500"}
-	rb1 = players.detect {|f| f["position"] == "RB" && f["dpp"] <= "500" }
-	rb2 = players.detect {|f| f["position"] == "RB" && f["name"] != rb1["name"] && f["dpp"] <= "500" }
-	te = players.detect {|f| f["position"] == "TE" && f["dpp"] <= "500"}
+#convert some elements to integer
 
-	qb1 = Player.new(qb["position"], qb["name"], qb["ppg"], qb["gp"], qb["matchup"], qb["salary"], qb["eqbp+"], qb["eqbpPts"], qb["dpp"])
-	wr1 = Player.new(wr1["position"], wr1["name"], wr1["ppg"], wr1["gp"], wr1["matchup"], wr1["salary"], wr1["exp+"], wr1["expPts"], wr1["dpp"])
-	wr2 = Player.new(wr2["position"], wr2["name"], wr2["ppg"], wr2["gp"], wr2["matchup"], wr2["salary"], wr2["exp+"], wr2["expPts"], wr2["dpp"])
-	wr3 = Player.new(wr3["position"], wr3["name"], wr3["ppg"], wr3["gp"], wr3["matchup"], wr3["salary"], wr3["exp+"], wr3["expPts"], wr3["dpp"])
-	rb1 = Player.new(rb1["position"], rb1["name"], rb1["ppg"], rb1["gp"], rb1["matchup"], rb1["salary"], rb1["exp+"], rb1["expPts"], rb1["dpp"])
-	rb2 = Player.new(rb2["position"], rb2["name"], rb2["ppg"], rb2["gp"], rb2["matchup"], rb2["salary"], rb2["exp+"], rb2["expPts"], rb2["dpp"])
-	te1 = Player.new(te["position"], te["name"], te["ppg"], te["gp"], te["matchup"], te["salary"], te["exp+"], te["expPts"], te["dpp"])
+players.each do |x|
+	x["expPts"] = Float(x["expPts"])
+	x["salary"] = Integer(x["salary"].gsub(/[^\d\.]/, '').to_f)
+end
 
-	firstRoster = Roster.new(qb1, rb1, rb2, wr1, wr2, wr3, te1)
-	puts "x"
+#load up positional arrays
 
-firstRoster.print()
-puts firstRoster.validate()
+wrArray = Array.new 
+qbArray = Array.new 
+rbArray = Array.new 
+teArray = Array.new 
+
+
+		players.each do |key|
+			if (key["position"] == "WR")
+				wrArray << key
+			elsif (key["position"] == "QB")
+				qbArray << key
+			elsif (key["position"] == "RB")
+				rbArray << key
+			elsif (key["position"] == "TE")
+				teArray << key
+			end
+		end
+
+		wrArray.to_a
+		qbArray.to_a
+		rbArray.to_a
+		teArray.to_a
+
+		wrArray.sort_by! {|k| k["expPts"]}.reverse!
+		qbArray.sort_by! {|k| k["expPts"]}.reverse!
+		rbArray.sort_by! {|k| k["expPts"]}.reverse!
+		teArray.sort_by! {|k| k["expPts"]}.reverse!
+
+wrTrio = Array.new
+
+
+wrTrio.each do |x|
+	puts x
+end
+
+
+
+
+
+
 
